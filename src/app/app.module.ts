48303d400a98cde9c505cf26ee662ca70f3289f0
VyacheslavKazarinov <satanigofplains@gmail.com>
2019-11-14 15:04:20 +0200

    feature: added material and post form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule, MatCardModule, MatButtonModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
+import {PostCreateComponent} from './posts/post-create/post-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
